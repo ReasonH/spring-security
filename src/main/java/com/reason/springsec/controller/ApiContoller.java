7deebb6eb131ea244ef90a06fdd34a6fac563715
YooHyuk Lee <azsxd34@naver.com>
2020-05-16 18:29:42 +0900

    Initial commit

---
+++ src/main/java/com/reason/springsec/controller/ApiContoller.java
@@ -0,0 +1,14 @@
+package com.reason.springsec.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiContoller {
+
+    @GetMapping("/api")
+    public String hello() {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/reason/springsec/domain/Account.java
@@ -0,0 +1,15 @@
+package com.reason.springsec.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Account {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    // write logic dto -> model
+}
